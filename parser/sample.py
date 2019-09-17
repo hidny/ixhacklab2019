26930b8e5deb46ed38f0c3396845285d29ec003e
Sohan Islam <sohan.islam@indexexchange.com>
2019-09-17 14:24:31 -0400

    [Parser]Loop through the first 20 scholarships

--- parser/sample.py
+++ parser/sample.py
@@ -2,13 +2,16 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-print("hello 1")
 
 quote_page = 'https://www.scholarshipscanada.com/Scholarships/ScholarshipSearch.aspx?type=ScholarshipName&s=toronto'
 page = urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
-name_box = soup.find('td', attrs={'class': 'Table-Standard-AwardName'})
 
-name = name_box.text.strip() # strip() is used to remove starting and trailing
-print("hello 2")
-print(name)
+name_box = soup.find_all('td', attrs={'class': 'Table-Standard-AwardName'})
+
+print("len: {}".format(len(name_box)))
+
+for i in name_box:
+	print(i.text.strip())
+	print()
+
