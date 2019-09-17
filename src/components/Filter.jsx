aa326cbb0db69a2e42e7095228f07ff32b8cf2f2
Anuruddha Hettiarachchi <anuruddha.hettiarachchi@indexexchange.com>
2019-09-17 15:52:14 -0400

    Filter page

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,54 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
 let Filter = () => (
-	<div>Filter</div>
+	<div >
+      <AppBar position="fixed" >
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Permanent drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        
+        variant="permanent"
+        anchor="left"
+      >
+        <div  />
+        <Divider />
+        <List>
+          {['Value', 'Dates', 'Region', 'Type'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main >
+        <div  />
+      </main>
+    </div>
 );
 export default Filter;
 
