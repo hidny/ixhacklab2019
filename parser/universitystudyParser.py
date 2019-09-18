d432ef489f25bd0be4beee771cca81a7f4ec6237
DongHyun <dongkun0904@gmail.com>
2019-09-18 10:45:36 -0400

    [Parser] Add scholarships from universitystudy to the database

--- parser/universitystudyParser.py
+++ parser/universitystudyParser.py
@@ -3,6 +3,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import pymysql
 
 
 BASE_URL = 'https://scholarships.universitystudy.ca/scholarships/?pg_scholarships='
@@ -13,40 +14,67 @@ def getSoup(link):
     soup = BeautifulSoup(page, 'html.parser')
     return soup
 
+# Open database connection
+db = pymysql.connect('10.5.29.7', 'ixhack', 'user', 'password')
 
-def parse():
-    j = 0
-    total = 0
-    
-    for i in range(1, 156):
-        url = BASE_URL + str(i)
-        soup = getSoup(url) 
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
 
-        awardNames = soup.find_all('tr')
-        awardNames = awardNames[1:]
+# Parse html into scholarship info
+j = 0
+total = 0
 
-        for award in awardNames:
-            total += 1
-            link = award.find('a')['href']
-            awardSoup = getSoup(link)
+for i in range(1, 156):
+    url = BASE_URL + str(i)
+    soup = getSoup(url) 
 
-            sidebar = awardSoup.find('div',attrs={'class': 'box'})
-            sidebarContent = sidebar.find_all('p')
+    awardNames = soup.find_all('tr')
+    awardNames = awardNames[1:]
+
+    for award in awardNames:
+        total += 1
+        link = award.find('a')['href']
+        awardSoup = getSoup(link)
+
+        sidebar = awardSoup.find('div',attrs={'class': 'box'})
+        sidebarContent = sidebar.find_all('p')
+        if(deadLine != "Closed"):
+            provider = sidebarContent[0].text.strip()
+            value = sidebarContent[1].text.strip()
             deadLine = sidebarContent[3].text.strip()
-            if(deadLine != "Closed"):
-                provider = sidebarContent[0].text.strip()
-                value = sidebarContent[1].text.strip()
-                print("deadline: {}, provider: {}, value: {}".format(deadLine,provider,value))
+            title = awardSoup.find('h1', attrs={'class': 'entry-title'}).text.strip()
+            print("title: {}".format(title))
+            print("link: {}".format(link))
+            print("deadline: {}, provider: {}, value: {}".format(deadLine, provider, value))
 
-                sidebarField = awardSoup.find('div',attrs={'class':'info-set'})
-                field = sidebarField.find('li')
-                print("field: {}".format(field.text.strip()))
-                
-                j = j+1
-                print()
-            if j > 100:
-                break
-    print("{}/{}".format(j, total))
+            sidebarField = awardSoup.find('div',attrs={'class':'info-set'})
+            field = sidebarField.find('li')
+            field = field.text.strip()
+            print("field: {}".format(field))
 
+            # Prepare SQL query to INSERT a record into the database.
+            sql = "INSERT INTO scholarship \
+                (link, school, award_name, amount, deadline, field_of_study) \
+                VALUES ('%s', '%s', '%s', '%d', '%s', '%s')" % \
+                (link, provider, title, value, deadLine, field)
+            #    VALUES ('%s', '%s', '%s', '%s', '%d', \
+            #        '%s', '%s', '%s', '%s', '%s', '%s', \
+            #            '%d', '%d', '%s' )" % \
+            try:
+                # Execute the SQL command
+                cursor.execute(sql)
+                # Commit your changes in the database
+                db.commit()
+            except:
+                # Rollback in case there is any error
+                db.rollback()
 
-parse()
+            j = j+1
+            print()
+        if j > 100:
+            break
+
+print("{}/{}".format(j, total))
+
+# disconnect from server
+db.close()
